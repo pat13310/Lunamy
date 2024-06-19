67b39b0de40db3bc4a3db260fc1a2b2ba7b2ad3e
Xenatronics <sarah13310@proton.me>
2024-06-19 15:15:08 +0200

    Version 1.43 : Ajout api avec contact

---
+++ Models/SendMail.cs
@@ -0,0 +1,85 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lunamy.Models
+{
+    class SendMail
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private static string apiUrl = "http://localhost:5000//common/api/v1"; // Remplacez avec l'URL de votre API
+        private static string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJmcmVzaCI6ZmFsc2UsImlhdCI6MTcxODcxMDgxOSwianRpIjoiZGI3OTY0MzktMjhiNi00ODMzLTk4MGYtZTI0ODFjM2RmMDJhIiwidHlwZSI6ImFjY2VzcyIsInN1YiI6eyJ1c2VybmFtZSI6InhlbiJ9LCJuYmYiOjE3MTg3MTA4MTksImNzcmYiOiJmYmI0NTE2OC03YzEzLTQyMDQtOTVmMS1lM2ZlYWYxNTQ4OGIiLCJleHAiOjE3MjY0ODY4MTl9.IGTcPlFMVyRmLc_AjPRA6bCrxDDODkUVaifLK5Zn3sk";
+
+        public static async Task<bool> Login(string username, string password)
+        {
+            var loginData = new
+            {
+                username = username,
+                password = password
+            };
+
+            var json = JsonConvert.SerializeObject(loginData);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+
+            try
+            {
+                var response = await client.PostAsync($"{apiUrl}/login", data);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = await response.Content.ReadAsStringAsync();
+                    var tokenData = JsonConvert.DeserializeObject<dynamic>(responseContent);
+                    token = tokenData.token;
+                    return true;
+                }
+                else
+                {
+                    // Gérer les erreurs ici
+                    return false;
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                // Gérer les exceptions de connexion ici
+                return false;
+            }
+        }
+
+        public static async Task SendMessage(string nom, string email, string sujet, string message)
+        {
+            if (string.IsNullOrEmpty(token))
+            {
+                // Connectez-vous d'abord pour obtenir le token
+                return;
+            }
+
+            var mailData = new
+            {
+                recipient = email,
+                subject = sujet,
+                body = message
+            };
+
+            var json = JsonConvert.SerializeObject(mailData);
+            var data = new StringContent(json, Encoding.UTF8, "application/json");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
+
+            try
+            {
+                var response = await client.PostAsync($"{apiUrl}/send-mail", data);
+                if (response.IsSuccessStatusCode)
+                {
+                    // Informer l'utilisateur que l'e-mail a été envoyé
+                }
+                else
+                {
+                    // Gérer les erreurs ici
+                }
+            }
+            catch (HttpRequestException e)
+            {
+                // Gérer les exceptions de connexion ici
+            }
+        }
+    }
+}
--- Views/Contact.xaml.cs
+++ Views/Contact.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using Lunamy.Models;
+using System.Windows.Controls;
 
 namespace Lunamy.Views
 {
@@ -7,9 +8,19 @@ namespace Lunamy.Views
     /// </summary>
     public partial class Contact : Page
     {
+        private SendMail sendMail = new();
         public Contact()
         {
             InitializeComponent();
+
+        }
+
+        private void OnSendMessage(object sender, System.Windows.RoutedEventArgs e)
+        {
+            string to = "webmaster@xendev.fr";
+            string msg = "Message de " + nom.Text + " [" + email.Text + "]" + "\n" + message.Text;
+            string subject = sujet.Text  ;
+            Task task = SendMail.SendMessage(nom.Text, to, subject,msg);
         }
     }
 }
